806ac4cfff0839206bc66f5e747dc081be0fa9a3
nramos9071 <nramos9071@gmail.com>
2024-10-02 19:43:02 -0500

    updated auth and added homeroutes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
+
+
+
+
+
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  module.exports = router;
