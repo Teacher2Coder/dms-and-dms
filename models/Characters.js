4c1abe127cea090777211cda80aeabbee5fd514b
HA_GO\antic <nmckinzie@smcm.edu>
2024-10-02 21:25:24 -0500

    added models

---
+++ models/Characters.js
@@ -0,0 +1,68 @@
+const { Model, DataTypes } = require('sequalize');
+const sequalize = require('../config/connection');
+
+class Characters extends Model { }
+
+Characters.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        class: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        skills: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        names: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        alignment: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        race: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        strength: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        dexterity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        constitution: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        intelligence: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        wisdom: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        charisma: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    },
+        {
+            sequelize,
+            timestamps: false,
+            freezeTableName: true,
+            underscored: true,
+            modelName: 'characters',
+          }
+);
+
+module.exports = Characters
