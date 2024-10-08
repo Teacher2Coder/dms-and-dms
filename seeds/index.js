b21ed253324a1177d222f91a291ca40d4bed578c
Ethan Owens <ethan@Ethans-MacBook-Pro.local>
2024-10-08 11:25:10 -0500

    adjusted user model and fixed image sizing issue

--- seeds/index.js
+++ seeds/index.js
@@ -4,7 +4,7 @@ const Characters = require('../models/Characters');
 const Quests = require('../models/Quest');
 const Stories = require('../models/Stories');
 const Rules = require('../models/Rules');
-const User = require('../models/user');
+const User = require('../models/User');
 
 // Import json data here
 // Uncomment the models when they are finshed and ready to go!
@@ -12,7 +12,7 @@ const characterSeeds = require('./characterSeeds.json');
 const questSeeds = require('./questSeeds.json');
 const storySeeds = require('./storySeeds.json');
 const ruleSeeds = require('./ruleSeeds.json');
-// const userSeeds = require('./userSeeds.json');
+const userSeeds = require('./userSeeds.json');
 
 
 // Uncomment bulkCreate methods when the seed.json files are finished and ready to go!
@@ -20,7 +20,7 @@ const seedDatabase = async () => {
     try {
         await sequelize.sync({ force: true });
 
-        // await User.bulkCreate(userSeeds);
+        await User.bulkCreate(userSeeds);
         await Characters.bulkCreate(characterSeeds);
         await Quests.bulkCreate(questSeeds);
         await Stories.bulkCreate(storySeeds);
