c162e0ef1a8e4b20d3db23ff51666ba1977ed534
Ethan Owens <ethan@Ethans-MacBook-Pro.local>
2024-10-08 09:00:55 -0500

    homepage now renders the three most recent ideas for each category

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
+const Character = require('../models/Characters');
+const Stories = require('../models/Stories');
+const Quests = require('../models/Quest');
+const Rules = require('../models/Rules');
 const User = require('../models/User');
 const withAuth = require('../utils/auth');
 
@@ -6,7 +10,49 @@ const withAuth = require('../utils/auth');
 // URL looks like this: localhost.3001/
 router.get('/', async (req, res) => {
   try {
-    res.render('homepage')
+
+    const characterData = await Character.findAll({
+      order: [['id', 'DESC']]
+    });
+    const characterDataPlain = characterData.map((characters) => characters.get({ plain: true }));
+
+    const firstCharacter = characterDataPlain[0];
+    const secondCharacter = characterDataPlain[1];
+    const thirdCharacter = characterDataPlain[2];
+
+    const questData = await Quests.findAll({
+      order: [['id', 'DESC']]
+    });
+    const questDataPlain = questData.map((quest) => quest.get({ plain: true }));
+
+    const firstQuest = questDataPlain[0];
+    const secondQuest = questDataPlain[1];
+    const thirdQuest = questDataPlain[2];
+
+    const storiesData = await Stories.findAll({
+      order: [['id', 'DESC']]
+    });
+    const storiesDataPlain = storiesData.map((story) => story.get({ plain: true }));
+
+    const firstStory = storiesDataPlain[0];
+    const secondStory = storiesDataPlain[1];
+    const thirdStory = storiesDataPlain[2];
+
+    const ruleData = await Rules.findAll({
+      order: [['id', 'DESC']]
+    });
+    const ruleDataPlain = ruleData.map((rule) => rule.get({ plain: true }));
+
+    const firstRule = ruleDataPlain[0];
+    const secondRule = ruleDataPlain[1];
+    const thirdRule = ruleDataPlain[2];
+
+    res.render('homepage', {
+      firstCharacter, secondCharacter, thirdCharacter,
+      firstQuest, secondQuest, thirdQuest,
+      firstStory, secondStory, thirdStory,
+      firstRule, secondRule, thirdRule
+     })
   } catch (err) {
     console.error(err);
     res.status(500).json(err);
