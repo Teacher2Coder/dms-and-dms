24e1af47153230bcbc243d13e96c1ba5c839894d
josephruiz18101 <joseph123920@gmail.com>
2024-10-10 22:46:32 -0500

    changed path

--- controllers/cardsroutes/index.js
+++ controllers/cardsroutes/index.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 
-const artifactRoutes = require('./cardsroutes/artifactRoutes');
-const blackRoutes = require('./cardsroutes/blackRoutes');
-const blueRoutes = require('./cardsroutes/blueRoutes');
-const cardsRoutes = require('./cardsroutes/cardsRoutes');
-const dungeonRoutes = require('./cardsroutes/dungeonRoutes');
-const greenRoutes= require('./cardsroutes/greenRoutes');
-const landRoutes = require('./cardsroutes/landRoutes');
-const multicoloredRoutes= require('./cardsroutes/multicoloredRoutes');
-const redRoutes = require('./cardsroutes/redRoutes');
-const whiteRoutes = require('./cardsroutes/whiteRoutes');
+const artifactRoutes = require('../cardsroutes/artifactRoutes');
+const blackRoutes = require('../cardsroutes/blackRoutes');
+const blueRoutes = require('../cardsroutes/blueRoutes');
+const cardsRoutes = require('../cardsroutes/cardsRoutes');
+const dungeonRoutes = require('../cardsroutes/dungeonRoutes');
+const greenRoutes= require('../cardsroutes/greenRoutes');
+const landRoutes = require('../cardsroutes/landRoutes');
+const multicoloredRoutes= require('../cardsroutes/multicoloredRoutes');
+const redRoutes = require('../cardsroutes/redRoutes');
+const whiteRoutes = require('../cardsroutes/whiteRoutes');
 
 router.use('/artifacts', artifactRoutes);
 router.use('/black', blackRoutes);
